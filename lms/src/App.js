b99ffe9382a95bcfe6b9106d9800247bd6131323
manish5124 <manishchouhan5124@gmail.com>
2023-12-18 20:25:23 +0530

    sign or login integration done

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
+import Home from './components/home/Home';
 import LoginSignup from './components/loginSignup/LoginSignup';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <LoginSignup/>
+       <Router>
+        <Routes>
+          <Route path="/" element={<LoginSignup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- lms/src/components/loginSignup/LoginSignup.jsx
+++ lms/src/components/loginSignup/LoginSignup.jsx
@@ -3,36 +3,73 @@ import './LoginSignup.css'
 
 import user_icon from '../Assets/person.png';
 import password_icon from '../Assets/password.png';
+import { useForm } from 'react-hook-form';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 function LoginSignup() {
-    
+  const navigate = useNavigate();
   const [action,setAction] = useState("Sign Up"); 
-  const [user, setUser] = useState();
-  const [password,SetPassword] = useState();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  
+
+  const onSubmit = (data) => {
+    console.log(data);
+    if(data!=null)
+    {
+        if(action==="Sign Up")
+        {
+            axios.post("http://localhost:8082/users/RegisterUser", data).then((response) => {
+            console.log("sign up response=>",response);
+            });
+        }
+        else
+        {
+            axios.post("http://localhost:8000/auth/login", data).then((response) => {
+                console.log("login response=>",response);
+                });
+                navigate('/home');
+        }
+    }
+  };
 
   return (
     <div className='container'>
-        <div className="header">
-            <div className="text">{action}</div>
-            <div className="underline"></div>
-        </div>
-        <div className="inputs">
-            <div className="input">
-                <img src={user_icon} alt="" srcset="" />
-                <input type="text" placeholder="Name" />
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
             </div>
-            <div className="input">
-                <img src={password_icon} alt="" srcset="" />
-                <input type="password" placeholder="password" />
+            <div className="inputs">
+                <div className="input form-control">
+                    <img src={user_icon} alt="" srcset="" />
+                    <input type="text" placeholder="Name" name="userName"  {...register("userName", {
+    required: true
+  })} />
+                </div>
+                <div className="input form-control">
+                    <img src={password_icon} alt="" srcset="" />
+                    <input type="password" placeholder="password"  name="password"   {...register("password", {
+              required: "Password is required.",
+              minLength: {
+                value: 6,
+                message: "Password should be at-least 6 characters."
+              }
+            })}/>
+                </div>
             </div>
+            {action==="Sign Up"?<div></div>: <div className="forget-password">Lost Password? <span>Click here</span> </div>}
+        
+            <div className="submit-container form-control">
+        <button type="submit" className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign up</button>
+        <button type="submit" className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</button>
         </div>
-        {action==="Sign Up"?<div></div>: <div className="forget-password">Lost Password? <span>Click here</span> </div>}
-       
-        <div className="submit-container">
-    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign up</div>
-    <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</div>
-    </div>
-
+    </form>
 
     </div>
   )
